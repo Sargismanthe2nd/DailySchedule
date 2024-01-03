fe80a8a0a6294778771607db5033f061927a6715
Jon <71684267+Sargismanthe2nd@users.noreply.github.com>
2024-01-03 15:41:23 -0800

    finished js

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,31 @@
-$(function () {
-  $('#currentDay').text(dayjs().format('dddd, MMMM D'));
+$('#currentDay').text(dayjs().format('dddd, MMMM D'));
 
+//saves the content inputed per each box.
+$('.saveBtn').click(function() {
+  var hourId = $(this).parent().attr('id');
+  var eventText = $(this).siblings('.description').val();
+  localStorage.setItem(hourId, eventText);
+});
 
+//changes the colour of the blocks with the current hour. 
+$('.time-block').each(function() {
+  var currentHour = dayjs().hour();
+  var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
-}
+  if (blockHour < currentHour) {
+    $(this).addClass('past').removeClass('present future');
+  } else if (blockHour === currentHour) {
+    $(this).addClass('present').removeClass('past future');
+  } else {
+    $(this).addClass('future').removeClass('past present');
+  }
+});
 
+//loads in saved content
+$('.time-block').each(function() {
+  var hourId = $(this).attr('id');
+  var savedEvent = localStorage.getItem(hourId);
+  if (savedEvent) {
+    $(this).find('.description').val(savedEvent);
+  }
+});
